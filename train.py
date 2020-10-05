acc81635437f24b39bd7ecfa20f4c8feeb856ebe
Simone Pietrogiacomi <sim.pietrogiacomi@gmail.com>
2020-10-05 18:27:19 +0200

    Reformat train

--- train.py
+++ train.py
@@ -1,4 +1,3 @@
-
 import argparse
 import os
 import tqdm
@@ -21,93 +20,93 @@ parser.add_argument('--dataset',
                     choices=ALL_DATASET_NAMES,
                     help="Dataset in {}".format(ALL_DATASET_NAMES),
                     required=True
-)
+                    )
 
 parser.add_argument('--model',
                     choices=ALL_MODEL_NAMES,
                     help="Model in {}".format(ALL_MODEL_NAMES)
-)
+                    )
 
 optimizers = ['Adam', 'SGD']
 parser.add_argument('--optimizer',
                     choices=optimizers,
                     default='Adam',
                     help="Optimizer in {}".format(optimizers)
-)
+                    )
 
 parser.add_argument('--max_epochs',
                     default=500,
                     type=int,
                     help="Number of epochs"
-)
+                    )
 
 parser.add_argument('--valid',
                     default=-1,
                     type=float,
                     help="Number of epochs before valid"
-)
+                    )
 
 parser.add_argument('--batch_size',
                     default=32,
                     type=int,
                     help="Number of samples in each mini-batch in SGD and Adam optimization"
-)
+                    )
 
 parser.add_argument('--weight_decay',
                     default=5e-4,
                     type=float,
                     help="L2 weight regularization of the optimizer"
-)
+                    )
 
 parser.add_argument('--learning_rate',
                     default=4e-4,
                     type=float,
                     help="Learning rate of the optimizer"
-)
+                    )
 
 parser.add_argument('--label_smooth',
                     default=0.1,
                     type=float,
                     help="Label smoothing for true labels"
-)
+                    )
 
 parser.add_argument('--decay1',
                     default=0.9,
                     type=float,
                     help="Decay rate for the first momentum estimate in Adam"
-)
+                    )
 
 parser.add_argument('--decay2',
                     default=0.999,
                     type=float,
                     help="Decay rate for second momentum estimate in Adam"
-)
+                    )
 
 parser.add_argument('--verbose',
                     default=True,
                     type=bool,
                     help="Verbose"
-)
+                    )
 
 parser.add_argument('--load',
                     type=bool,
                     required=False,
                     help="Path to the model to load"
-)
+                    )
 
 parser.add_argument('--batch_norm',
                     default=False,
                     type=bool,
                     required=False,
                     help="If True, apply batch normalization",
-)
+                    )
 
 parser.add_argument('--vgg-cfg',
                     type=str,
                     default='A',
                     help="VGG architecture config",
                     required=False
-)
+                    )
 
 args = parser.parse_args()
 
@@ -123,7 +122,7 @@ dataset = BasicDataset(imgs_dir=imgs_dir, masks_dir=masks_dir)
 print("Initializing model...")
 model = LogoDetectionModel(dataset=dataset,
                            batch_norm=args.batch_norm,
-                           vgg_cfg=args.vgg_cfg) 
+                           vgg_cfg=args.vgg_cfg)
 model.to('cuda')
 if args.load is not None:
     model.load_state_dict(torch.load(model_path))
@@ -133,7 +132,8 @@ print("Training model...")
 # Optimizer selection
 # build all the supported optimizers using the passed params (learning rate and decays if Adam)
 supported_optimizers = {
-    'Adam': optim.Adam(params=model.parameters(), lr=args.learning_rate, betas=(args.decay_adam_1, args.decay_adam_2), weight_decay=args.weight_decay),
+    'Adam': optim.Adam(params=model.parameters(), lr=args.learning_rate, betas=(args.decay_adam_1, args.decay_adam_2),
+                       weight_decay=args.weight_decay),
     'SGD': optim.SGD(params=model.parameters(), lr=args.learning_rate, weight_decay=args.weight_decay)
 }
 
@@ -154,12 +154,11 @@ def train(model,
           batch_size,
           max_epochs,
           save_path,
-          evaluate_every, 
+          evaluate_every,
           optimizer,
           label_smooth,
           verbose):
-
-    if(init_normal==True):
+    if (init_normal == True):
         weights_init(model)
 
     for e in range(max_epochs):
@@ -173,18 +172,18 @@ def train(model,
         #     self.model.eval()
         #     with torch.no_grad():
         #         mrr, h1 = self.evaluator.eval(samples=valid_samples, write_output= False) 
-            
+
         #     # Metrics printing
         #     print("\tValidation: %f" % h1)
-            
+
         # if save_path is not None:
         #     print("\tSaving model...")
         #     torch.save(self.model.state_dict(), save_path)
         # print("\tDone.")
 
     def epoch(self,
-        batch_size: int,
-        train_samples: np.array):
+              batch_size: int,
+              train_samples: np.array):
 
         n_samples = train_samples.shape[0]
 
@@ -192,7 +191,7 @@ def train(model,
         train_samples = torch.from_numpy(train_samples).cuda()
         randomized_samples = train_samples[torch.randperm(n_samples), :]
 
-        loss = torch.nn.BCELoss() * (1/256*256)
+        loss = torch.nn.BCELoss() * (1 / 256 * 256)
 
         # Training over batches
 
@@ -218,7 +217,7 @@ def train(model,
 
         # # Label smoothing (?)
         # truth = (1.0 - self.label_smooth)*truth + (1.0 / truth.shape[1])
-        
+
         # Compute loss
         l = loss(prediction, truth)
 
@@ -227,16 +226,15 @@ def train(model,
         l.backward()
         self.optmizer.step()
 
-        #return loss
+        # return loss
         return l
+
 
 print("\nEvaluating model...")
 model.eval()
 mrr, h1 = Evaluator(model=model).eval(samples=dataset.test_samples, write_output=False)
 print("\tTest Hits@1: %f" % h1)
 print("\tTest Mean Reciprocal Rank: %f" % mrr)
-
-
 
 # class Optimizer:
 
@@ -277,10 +275,10 @@ print("\tTest Mean Reciprocal Rank: %f" % mrr)
 #             self.model.eval()
 #             with torch.no_grad():
 #                 mrr, h1 = self.evaluator.eval(samples=valid_samples, write_output= False) 
-            
+
 #             # Metrics printing
 #             print("\tValidation: %f" % h1)
-            
+
 #         if save_path is not None:
 #             print("\tSaving model...")
 #             torch.save(self.model.state_dict(), save_path)
@@ -322,7 +320,7 @@ print("\tTest Mean Reciprocal Rank: %f" % mrr)
 
 #         # # Label smoothing (?)
 #         # truth = (1.0 - self.label_smooth)*truth + (1.0 / truth.shape[1])
-        
+
 #         # Compute loss
 #         l = loss(prediction, truth)
 
@@ -333,5 +331,3 @@ print("\tTest Mean Reciprocal Rank: %f" % mrr)
 
 #         #return loss
 #         return l
-        
-
